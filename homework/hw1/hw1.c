fd1d892d6d73a9c68bac059d412fbfb999b64628
GrichukSlava <grichuk.vi@mail.ru>
2023-08-11 18:28:24 +0300

    небольшой фикс певрой домашней работы

---
+++ homework/hw1/hw1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Item {
+    int val;
+    struct Item *next;
+};
+
+
+int ordered_list( struct Item *head) {
+    if( head->next == NULL || head == NULL){
+        return -1;
+    }
+    struct Item *prev = head;
+    struct Item *curr = head->next;
+    while (curr != NULL) {
+        if (prev->val > curr->val) {
+            return -1;
+        }
+        prev = prev->next;
+        curr = curr->next;
+        }
+    return 1;
+}
+
+
+int main() {
+    struct Item *item1 =  malloc(sizeof(struct Item));
+    item1->val = 1;
+    item1->next = NULL;
+
+    int res = ordered_list(item1);
+    printf("ответ -%d", res );
+    return 0;
+}
